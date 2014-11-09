90e0eb0c2355cecae0346449642c344051f47fc3
Benji Kay <okaybenji@gmail.com>
2014-11-08 18:11:18 -0600

    add socket.io

--- node.js
+++ node.js
@@ -1,13 +1,18 @@
-var express = require('express')
+var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var port = 8080;
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.use(express.static(__dirname + '/')); 
+app.use(express.static(__dirname + '/'));
+
+io.on('connection', function(socket) {
+    console.log('user connected with IP',socket.handshake.address);
+});
 
 http.listen(port, function(){
     console.log('listening on port',port);
